07071f4fe83672534b63e099eb7eb6c2cc01cce7
SUMIT <sumitdogra3236@gmail.com>
2021-06-02 13:23:19 +0530

    Your Location

---
+++ README.md
@@ -0,0 +1,4 @@
+# To run the program : Run main.js > 
+
+Just by Provding Input Location, Program is capable of to provide the general information about your loaction, as Distance of Specific Location from your location, distance of Airport, Railway Stations, Malls and Shopping Centers, Cinema Halls, PVR's,  Hotels and Restaurants.
+The module used is Puppeteer and the scrapping of Google Maps.
---
+++ main.js
@@ -0,0 +1,56 @@
+/*
+Project to know about your surrounding locations, Universities, Hospitals, Railway Stations, Malls and Shopping Centers, Airports
+*/
+const location = "Nirwna Greens 4 , Nirwana, Mohali";
+const puppeteer = require("puppeteer");
+const {gettabledetails, getdist}=require("./driver");
+let detailsOF = ["Chandigarh University", "Hotels", "Hospitals ", "Shopping Centers", "Restaurants", "Bus stand", "PVR",];
+(async function () {
+    try {
+
+        let browser = await puppeteer.launch({
+            defaultViewport: null,
+            headless: false,
+            args: ["--start-maximized"]
+        })
+
+        console.log(`Below you wil find the data regarding public amenities near your desired location: ${location}`);
+        let tab = await browser.newPage();
+        await tab.goto("https://www.google.com/maps");
+        await tab.waitForNavigation({ visible: true });
+        await tab.type("#searchboxinput", location, { delay: 100 });
+        await tab.keyboard.press("Enter");
+        await tab.waitForNavigation({ visible: true, waitUntil: "networkidle0" });
+        await tab.waitForSelector("div[data-value='Nearby']");
+
+        let dist1=await getdist(tab, "Airport");
+
+        console.log(`
+         Distance from your location to Airport is ${dist1}
+        `);
+
+        let dist2=await getdist(tab, "Railway Station");
+        
+        console.log(`
+         Distance from your location to Railway Station is ${dist2}
+        `);
+
+        await tab.waitForSelector("div[data-value='Nearby']");
+        await tab.click("button[data-value='Nearby']");
+        await tab.waitForNavigation({ visible: true, waitUntil: "networkidle0" });
+        for (let i = 0; i <= detailsOF.length; i++) {
+            if (i == 0) {
+                let detailsarr = await gettabledetails(tab, detailsOF[i]);
+            } else if (i == detailsOF.length) { }
+            else {
+                console.log(`Details of (top 5) ${detailsOF[i - 1]} are below:`);
+                let detailsarr = await gettabledetails(tab, detailsOF[i]);
+                console.table(detailsarr);
+            }
+        }
+        await tab.waitForTimeout();
+        await browser.close();
+    } catch (err) {
+        console.log(err);
+    }
+})();
